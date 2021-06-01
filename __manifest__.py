8ef2b1280c4155c227c1bbc488875bc305679339
image4ever <paulofreire@paulofreire.biz>
2021-06-01 18:48:42 -0300

    ajustes no __manifest__

--- __manifest__.py
+++ __manifest__.py
@@ -2,13 +2,5 @@
  'description': 'Library books, members and book borrowing.',
  'author': 'Daniel Reis',
  'depends': ['base'],
- 'data': [
-    'security/library_security.xml',
-    'security/ir.model.access.csv',
-    'views/library_menu.xml',
-    'views/book_view.xml',
-    'views/book_list_template.xml',
- ],
- 'application': True,
- 'installable': True,
+ 'application': True
  }
